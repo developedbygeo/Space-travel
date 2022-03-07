0b1bab09ce69b4c8aa5c3c5046d6ff40241b20f1
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 22:02:58 +0200

    chore: consolidating image imports/exports into a single file

---
+++ src/shared/images.ts
@@ -0,0 +1,22 @@
+import homeMobile from '../assets/home/background-home-mobile.jpg';
+import homeTablet from '../assets/home/background-home-tablet.jpg';
+import homeDesktop from '../assets/home/background-home-desktop.jpg';
+import destinationMobile from '../assets/destination/background-destination-mobile.jpg';
+import destinationTablet from '../assets/destination/background-destination-tablet.jpg';
+import destinationDesktop from '../assets/destination/background-destination-desktop.jpg';
+
+import crewMobile from '../assets/crew/background-crew-mobile.jpg';
+import crewTablet from '../assets/crew/background-crew-tablet.jpg';
+import crewDesktop from '../assets/crew/background-crew-desktop.jpg';
+
+export default [
+    homeMobile,
+    homeTablet,
+    homeDesktop,
+    destinationMobile,
+    destinationTablet,
+    destinationDesktop,
+    crewMobile,
+    crewTablet,
+    crewDesktop,
+];
