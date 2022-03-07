5f03a0ad4361b767e9ae65bb7dc56c8e9e20e4f7
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 22:03:24 +0200

    chore: adding the theme

---
+++ src/shared/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+    typography: {
+        lineHeight: {
+            body: '18px',
+        },
+    },
+    colors: {
+        mainBg: '#0b0d17',
+        text: '#ffffff',
+        accent: '#d0d6f9',
+    },
+};
+
+export { myTheme };
