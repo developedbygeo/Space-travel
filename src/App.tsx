8745c6c0301dc39e831cae8de70d6e10f306b4e2
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 22:02:00 +0200

    feat: adding global styles, header and using the useBackground custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import GlobalStyle from './shared/globalStyle';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { AppContainer } from './components/UI/Container';
+
+import useBackground from './hooks/useBackground';
+
+const App = () => {
+    const img = useBackground();
+
+    return (
+        <>
+            <GlobalStyle />
+            <AppContainer img={img}>
+                <Header />
+            </AppContainer>
+        </>
+    );
+};
 
 export default App;
