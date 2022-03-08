617674f176361e83731a6db4ac5c7bae55cdc7be
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-09 00:38:01 +0200

    feat: adding the base home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+    return (
+        <section>
+            <div className="text">
+                <p className="intro">SO, YOU WANT TO TRAVEL TO</p>
+                <h1>SPACE</h1>
+                <p className="desc">
+                    Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not
+                    hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this
+                    world experience!
+                </p>
+            </div>
+            <div className="cta-wrap">
+                <button>EXPLORE</button>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
