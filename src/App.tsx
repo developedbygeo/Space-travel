c288e5301c65551e7798e2d13971d72c3c371c12
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-09 00:27:02 +0200

    feat: adding base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './shared/globalStyle';
 import Header from './components/Header/Header';
 
-import { AppContainer } from './components/UI/Container';
+import { AppContainer } from './components/UI/Container.styled';
+import Home from './pages/Home';
 
 import useBackground from './hooks/useBackground';
 
 const App = () => {
-    const img = useBackground();
+    const { img } = useBackground();
 
     return (
         <>
             <GlobalStyle />
             <AppContainer img={img}>
                 <Header />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<Home />} />
+                    </Routes>
+                </main>
             </AppContainer>
         </>
     );
