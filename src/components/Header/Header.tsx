3814b863a86a1cd6a623552dbac92533ba35b768
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-09 00:28:46 +0200

    feat: adding the mobile header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import useBackground from '../../hooks/useBackground';
+
 import { ReactComponent as Logo } from '../../assets/common/logo.svg';
 import StyledHeader from './Header.styled';
-import { HiMenu } from 'react-icons/hi';
+import Nav from './Nav';
+import { GoThreeBars, GoX } from 'react-icons/go';
+import { UnstyledButton } from '../UI/Button.styled';
 
 const Header = () => {
+    const { width } = useBackground();
+    const [navIsEnabled, setNavIsEnabled] = useState(false);
+
+    const navClass = width < 768 ? 'mobile-nav' : width > 768 && width < 1025 ? 'tablet-nav' : 'desktop-nav';
+
+    const navToggleHandler = () => {
+        setNavIsEnabled((prevState) => !prevState);
+    };
+
+    const buttonContent = !navIsEnabled ? <GoThreeBars /> : <GoX />;
+
     return (
         <StyledHeader>
-            <Logo />
-            <button title="Toggle Menu" className="menu">
-                <HiMenu />
-            </button>
+            <Link to="/">
+                <Logo className="logo" />
+            </Link>
+            <UnstyledButton onClick={navToggleHandler} title="Toggle Menu" className="menu">
+                {buttonContent}
+            </UnstyledButton>
+            {navIsEnabled && <Nav className={navClass} />}
         </StyledHeader>
     );
 };
