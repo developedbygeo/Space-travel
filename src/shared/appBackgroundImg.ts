a1d46fe05feadb362204b5a9287031b7a5a87b99
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 02:36:06 +0200

    style: adding the variableBackground logic & snippet

---
+++ src/shared/appBackgroundImg.ts
@@ -0,0 +1,54 @@
+import { css } from 'styled-components';
+import { homeImg, destinationImg, crewImg, techImg } from './images';
+
+export const matchRegex = (operand: string, regex: RegExp, defaultValue: string) => {
+    const match = operand.match(regex);
+    if (match && match.length) {
+        return match[0].replace('/', '');
+    }
+    return defaultValue;
+};
+
+const variableBackground = css`
+    & > .home {
+        background-image: url(${homeImg[0]});
+
+        @media (min-width: 800px) {
+            background-image: url(${homeImg[1]});
+        }
+        @media (min-width: 1025px) {
+            background-image: url(${homeImg[2]});
+        }
+    }
+    & > .destination {
+        background-image: url(${destinationImg[0]});
+
+        @media (min-width: 800px) {
+            background-image: url(${destinationImg[1]});
+        }
+        @media (min-width: 1025px) {
+            background-image: url(${destinationImg[2]});
+        }
+    }
+    & > .crew {
+        background-image: url(${crewImg[0]});
+
+        @media (min-width: 800px) {
+            background-image: url(${crewImg[1]});
+        }
+        @media (min-width: 1025px) {
+            background-image: url(${crewImg[2]});
+        }
+    }
+
+    & > .technology {
+        background-image: url(${techImg[0]});
+        @media (min-width: 800px) {
+            background-image: url(${techImg[1]});
+        }
+        @media (min-width: 1025px) {
+            background-image: url(${techImg[2]});
+        }
+    }
+`;
+export default variableBackground;
