a387cbfd60e5c5596109a7199199e3673f07ffdc
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 02:35:36 +0200

    chore: code cleanup

--- src/components/UI/Container.styled.ts
+++ src/components/UI/Container.styled.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 type AppContainerProps = {
+    className: string;
     img?: string;
     children?: React.ReactNode;
 };
@@ -9,6 +10,7 @@ type AppContainerProps = {
 export const AppContainer = styled.div<AppContainerProps>`
     height: 100vh;
     width: 100vw;
-    background: url(${(props) => props.img}) center no-repeat;
+    background-position: center;
+    background-repeat: no-repeat;
     background-size: cover;
 `;
