5f7b3e2b01e1cbef958ca8ab489c99cf913c93ce
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 02:31:36 +0200

    refactor: removing unnecessary code - hook now only returns the width

--- src/hooks/useBackground.ts
+++ src/hooks/useBackground.ts
@@ -1,15 +1,7 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
-import lookup from '../shared/images';
-import homeDesktop from '../assets/home/background-home-desktop.jpg';
 
 const useBackground = () => {
     const [windowWidth, setwindowWidth] = useState(0);
-    const { pathname } = useLocation();
-    const location = pathname.replace('/', '');
-
-    const device = windowWidth < 480 ? 'mobile' : windowWidth > 480 && windowWidth < 900 ? 'tablet' : 'desktop';
 
     useEffect(() => {
         const handleResize = () => {
@@ -20,12 +12,7 @@ const useBackground = () => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    const lookupMatch = lookup.find((obj) => obj.location === location);
-
-    if (lookupMatch) {
-        return { width: windowWidth, img: lookupMatch[device] };
-    }
-    return { width: windowWidth, img: homeDesktop };
+    return windowWidth;
 };
 
 export default useBackground;
