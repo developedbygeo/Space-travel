f01d9c2cf7d26ee346a2fedc292bd88eb1ba09fc
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:35:28 +0200

    refactor: extracting interaction snippets

--- src/shared/mixins.ts
+++ src/shared/mixins.ts
@@ -19,57 +19,6 @@ export const maxContainer = css`
     width: 100%;
 `;
 
-export const colorInteract = css`
-    transition: all 100ms ease-in-out;
-    position: relative;
-    @media (hover: hover) {
-        &:hover {
-            color: ${({ theme }) => theme.colors.highlight};
-        }
-    }
-    &:active {
-        transform: scale(1.1);
-    }
-`;
-
-export const interactAndHover = css`
-    ${colorInteract};
-    @media (hover: hover) {
-        &::after {
-            position: absolute;
-            content: '';
-            left: 0%;
-            bottom: -3px;
-            width: 100%;
-            height: 0.75px;
-            transform-origin: right;
-            transform: scaleX(0);
-            background: ${({ theme }) => theme.colors.accent};
-            transition: transform 350ms ease;
-        }
-        &:hover {
-            &::after {
-                transform-origin: left;
-                transform: scaleX(1);
-            }
-        }
-    }
-`;
-
-export const showActive = css`
-    color: ${({ theme }) => theme.colors.accent};
-    font-weight: 600;
-    position: relative;
-    &::before {
-        position: absolute;
-        content: '';
-        width: 100%;
-        height: 0.25rem;
-        bottom: -0.5rem;
-        background: ${({ theme }) => theme.colors.accent};
-    }
-`;
-
 export const customContainer = (h: string, w: string) => css`
     height: ${h}%;
     width: ${w}%;
