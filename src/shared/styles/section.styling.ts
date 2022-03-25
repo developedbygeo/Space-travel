2d2670730304490206df210850c33c1a5f218242
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:37:11 +0200

    chore: code cleanup

--- src/shared/styles/section.styling.ts
+++
@@ -1,8 +0,0 @@
-import { css } from 'styled-components';
-import { maxContainer } from '../mixins';
-
-export const baseSectionStyling = css`
-    padding: 4vh;
-    ${maxContainer}
-    background: ${({ theme }) => theme.colors.mainBg};
-`;
