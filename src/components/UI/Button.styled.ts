35f53ae5534fa9a35d39dec4a4b7894e36e1dc8d
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:31:38 +0200

    feat: adding CTA

--- src/components/UI/Button.styled.ts
+++ src/components/UI/Button.styled.ts
@@ -1,6 +1,8 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components/macro';
 
-import { colorInteract, resetDefaults } from '../../shared/mixins';
+import { resetDefaults } from '../../shared/mixins';
+import { colorInteract, interactCta } from '../../shared/styles/interactive.styles';
+import { elevationThree } from '../../shared/styles/shadows.styles';
 
 import { StyledProps } from '../../shared/models/props.model';
 
@@ -10,9 +12,24 @@ export const UnstyledButton = styled.button<StyledProps>`
     ${colorInteract}
     ${resetDefaults}
 
-    color: ${({ theme }) => theme.colors.text};
+    color: rgb(${({ theme }) => theme.colors.text});
     svg {
         height: 100%;
         width: 100%;
     }
+`;
+
+export const CtaButton = styled.button<StyledProps>`
+    ${resetDefaults}
+    width: 30rem;
+    height: 30rem;
+    max-width: 30vw;
+    max-height: 30vw;
+    border-radius: 50%;
+    color: rgb(${({ theme }) => theme.colors.mainBg});
+    background: rgb(${({ theme }) => theme.colors.text});
+    font-family: ${({ theme }) => theme.typography.fonts.heading};
+    letter-spacing: 0.125rem;
+    ${interactCta}
+    ${elevationThree}
 `;
