94cfd6675f81f9cbb8e75da0cee9bba21472117e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:31:56 +0200

    style: adding the styled section wrapper

--- src/components/UI/Container.styled.ts
+++ src/components/UI/Container.styled.ts
@@ -1,5 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
+
+import landingStyling from '../../shared/dedicated-styles/landing.styles';
+import destinationStyling from '../../shared/dedicated-styles/destination.styles';
+import { flexMixin } from '../../shared/mixins';
+import { LayoutProps } from '../../shared/models/utils.model';
+import { getLayout } from '../../shared/utils';
 
 type AppContainerProps = {
     className: string;
@@ -13,4 +19,15 @@ export const AppContainer = styled.div<AppContainerProps>`
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+`;
+
+export const StyledSection = styled.section<LayoutProps>`
+    ${flexMixin('space-between', 'center', 'column')};
+    ${getLayout};
+    height: 90%;
+    width: 90%;
+    margin: auto;
+
+    ${landingStyling};
+    ${destinationStyling};
 `;
