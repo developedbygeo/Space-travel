06104eb9e3b035c1e64ef74030f45ff6f60fcdf9
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:35:52 +0200

    style: adding font settings and refactoring colors

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -2,16 +2,21 @@ import { DefaultTheme } from 'styled-components';
 
 const myTheme: DefaultTheme = {
     typography: {
+        fonts: {
+            heading: "'Bellefair', serif",
+            subheading: "'Barlow Condensed', sans-serif",
+            body: "'Barlow', sans-serif",
+        },
         lineHeight: {
             body: '18px',
         },
     },
     colors: {
-        mainBg: '#0b0d17',
-        text: '#ffffff',
-        accent: '#d0d6f9',
-        highlight: '#A4B0F4',
-        nav: '#979797',
+        mainBg: '11, 13, 23' || '#0b0d17',
+        text: '255, 255, 255' || '#ffffff',
+        accent: '208, 214, 249' || '#d0d6f9',
+        highlight: '164, 176, 244' || '#a4b0f4',
+        nav: '151, 151, 151' || '#979797',
     },
 };
 
