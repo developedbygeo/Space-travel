0798f1aad4ac7418dbb4fdeb2a3c14359e0364b4
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-25 02:37:02 +0200

    style: adding shadow snippets

---
+++ src/shared/styles/shadows.styles.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+export const elevationOne = css`
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+`;
+
+export const elevationTwo = css`
+    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+`;
+
+export const elevationThree = css`
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+`;
