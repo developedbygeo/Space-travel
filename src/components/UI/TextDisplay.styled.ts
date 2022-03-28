6b4a95142ef319031b2214289550673025463f9d
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-29 01:30:26 +0300

    style: adding the styled text display

---
+++ src/components/UI/TextDisplay.styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/macro';
+
+import { flexMixin } from '../../shared/mixins';
+
+const StyledTextDisplay = styled.section`
+    height: 100%;
+    .text-content {
+        margin-block: 2vh;
+        min-height: 22vh;
+    }
+
+    /* reserved for the destination page (avg distance/travel time) */
+    .additional-info {
+        padding-top: 3vh;
+        width: 100%;
+        align-self: flex-start;
+        justify-self: center;
+        text-align: center;
+        border-top: 0.5px solid rgb(${({ theme }) => theme.colors.darkAccent});
+        ${flexMixin('center', 'center', 'column')};
+        gap: 1vh;
+    }
+`;
+
+export default StyledTextDisplay;
