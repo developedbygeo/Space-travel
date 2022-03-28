e8dddb699e8246c91dde608b0f49ca2a2d6fbdfb
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-29 01:28:46 +0300

    feat: adding the component to render the main text

---
+++ src/components/TextDisplay/MainText.tsx
@@ -0,0 +1,21 @@
+import { BigHeading, MediumHeading, SmallSubheading, Description } from '../UI/Text.styled';
+
+type MainTextProps = {
+    details: {
+        heading: string;
+        subheading?: string;
+        body: string;
+    };
+};
+
+const MainText = ({ details: { heading, subheading, body } }: MainTextProps) => {
+    return (
+        <article className="text-content">
+            {subheading && <SmallSubheading as="span">{subheading}</SmallSubheading>}
+            <BigHeading>{heading}</BigHeading>
+            <Description>{body}</Description>
+        </article>
+    );
+};
+
+export default MainText;
