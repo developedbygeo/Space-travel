57f74010d99470addba65957c426dcda069b23d0
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-29 01:36:52 +0300

    style: page-specific styling

--- src/shared/dedicated-styles/destination.styles.ts
+++ src/shared/dedicated-styles/destination.styles.ts
@@ -1,30 +1,17 @@
 import { css } from 'styled-components';
 
-import { flexMixin, gridMixin } from '../mixins';
-import { interactAndHover, showActive } from '../styles/interactive.styles';
+import { gridMixin } from '../mixins';
+
+// TODO could consolidate all extra page styles here
 
 const destinationStyling = css`
-    &.destination {
+    &.destination,
+    &.crew {
         height: 100%;
-        ${gridMixin('1fr', '0.8fr 0.43fr 0.5fr')};
-        gap: 2vh;
+        ${gridMixin('1fr', 'auto')};
         padding-block: 0;
-    }
-    ul {
-        ${flexMixin('center', 'center', 'row')};
-        gap: 2rem;
-        text-transform: uppercase;
-        font-family: ${({ theme }) => theme.typography.fonts.subheading};
-        li {
-            cursor: pointer;
-            letter-spacing: 0.15rem;
-            ${interactAndHover};
-            &::after {
-                height: 0.25rem;
-            }
-        }
-        .active {
-            ${showActive};
+        .text-content {
+            text-align: center;
         }
     }
 `;
