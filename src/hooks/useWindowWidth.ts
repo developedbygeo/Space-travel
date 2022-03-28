780f4787a4531193e9720bece91afc77ea45d030
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-29 01:31:29 +0300

    chore: renaming for clarity

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const useWindowWidth = () => {
+    const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setwindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowWidth;
+};
+
+export default useWindowWidth;
