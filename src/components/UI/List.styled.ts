fd72848c3f6e48d8a21f3cd312740d9d76e61324
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-29 01:29:36 +0300

    style: adding the styled ul

---
+++ src/components/UI/List.styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/macro';
+
+import { flexMixin } from '../../shared/mixins';
+import { interactAndHover, showActive } from '../../shared/styles/interactive.styles';
+
+const StyledList = styled.ul`
+    ${flexMixin('center', 'center', 'row')};
+    gap: 2rem;
+    text-transform: uppercase;
+    font-family: ${({ theme }) => theme.typography.fonts.subheading};
+    li {
+        cursor: pointer;
+        letter-spacing: 0.15rem;
+        ${interactAndHover};
+        padding: 0.75rem;
+        &::after {
+            height: 0.25rem;
+        }
+    }
+    .active {
+        ${showActive};
+    }
+`;
+
+export default StyledList;
