49bdec7f56fbbf55caf3768500e1f42fe712c542
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-30 03:31:27 +0300

    style: adding tablet and desktop media queries

--- src/shared/dedicated-styles/destination.styles.ts
+++ src/shared/dedicated-styles/destination.styles.ts
@@ -1,17 +1,94 @@
 import { css } from 'styled-components';
 
-import { gridMixin } from '../mixins';
+import { devices } from '../breakpoints';
+import { gridMixin, flexMixin } from '../mixins';
 
-// TODO could consolidate all extra page styles here
+// TODO consolidate media queries into one snippet
 
 const destinationStyling = css`
     &.destination,
     &.crew {
         height: 100%;
-        ${gridMixin('1fr', 'auto')};
+        width: 95%;
+        ${gridMixin('1fr', '0.2fr 0.8fr 0.1fr 1.5fr')};
+        gap: 1.5vh;
         padding-block: 0;
         .text-content {
             text-align: center;
+        }
+        ul {
+            align-items: center;
+            margin-bottom: 1vh;
+        }
+        button,
+        li {
+            padding: 1rem;
+        }
+    }
+
+    @media ${devices.tablet} {
+        &.destination,
+        &.crew {
+            padding-block: 2vh;
+            .header {
+                justify-content: flex-start;
+            }
+            h1 {
+                font-size: 6rem;
+            }
+            .text-content {
+                width: 80%;
+                margin-inline: auto;
+            }
+            .additional-info {
+                width: 80%;
+                margin-inline: auto;
+                padding-top: 5vh;
+                ${flexMixin('space-around', 'center', 'row')};
+            }
+        }
+    }
+    @media ${devices.laptop} {
+        &.destination {
+            ${gridMixin('repeat(2, 1fr)', '0.1fr 0.1fr 1fr')}
+
+            .img-cont {
+                grid-area: 2/1/4/2;
+                img {
+                    height: 50vh;
+                }
+            }
+            .header {
+                grid-area: 1/1/3/2;
+                width: 80%;
+                margin-inline: auto;
+                align-self: flex-end;
+                padding-bottom: 3.25vh;
+            }
+            ul {
+                width: 75%;
+                justify-content: space-between;
+                align-items: flex-start;
+                margin-inline: auto;
+                grid-area: 2/2/3/4;
+            }
+            section {
+                height: auto;
+                width: 95%;
+                margin: auto;
+                grid-area: 2/2/4/2;
+            }
+            .text-content {
+                margin-block: 10vh;
+            }
+            .additional-info {
+                justify-content: space-between;
+            }
+            h1,
+            p,
+            h4 {
+                text-align: left;
+            }
         }
     }
 `;
