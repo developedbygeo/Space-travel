66937f630efa5f6a06f216c9afaa0864e9dc8bad
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-31 00:30:03 +0300

    feat: adding 404 route and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
+import React, { Suspense } from 'react';
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
 import GlobalStyle from './shared/globalStyle';
 import Header from './components/Header/Header';
-import Home from './pages/Home';
-import Destination from './pages/Destination';
-import Crew from './pages/Crew';
-import Technology from './pages/Technology';
+import Skeleton from './components/UI/Skeleton';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Destination = React.lazy(() => import('./pages/Destination'));
+const Crew = React.lazy(() => import('./pages/Crew'));
+const Technology = React.lazy(() => import('./pages/Technology'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 import { matchRegex } from './shared/appBackgroundImg';
 import { AppContainer } from './components/UI/Container.styled';
 
-const regex = /^\/[a-z]+/;
+const regex = /^\/[a-z]+$/;
+
+const acceptedPaths = ['home', 'destination', 'crew', 'technology'];
 
 const App = () => {
     const { pathname } = useLocation();
     const derivedClassname = matchRegex(pathname, regex, 'home');
+    const notFoundBackground = !acceptedPaths.includes(pathname.replace('/', '')) ? 'notFound' : '';
 
     return (
         <>
             <GlobalStyle />
-            <AppContainer className={`appcont ${derivedClassname}`}>
+            <AppContainer className={`appcont ${derivedClassname} ${notFoundBackground}`}>
                 <Header />
                 <main>
-                    <Routes>
-                        <Route path="/" element={<Navigate to="/home" />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/destination" element={<Destination />} />
-                        <Route path="/crew" element={<Crew />} />
-                        <Route path="/technology" element={<Technology />} />
-                    </Routes>
+                    <Suspense fallback={<Skeleton />}>
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/home" />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/destination" element={<Destination />} />
+                            <Route path="/crew" element={<Crew />} />
+                            <Route path="/technology" element={<Technology />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </Suspense>
                 </main>
             </AppContainer>
         </>
