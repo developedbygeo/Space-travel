74a65ec0fa1a518f878ed065ada2f46f5898c201
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-31 00:32:08 +0300

    refactor: code cleanup due to useData

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,38 +1,32 @@
-import { useState } from 'react';
+import useData from '../hooks/useData';
+import { TechnologyDataType } from '../shared/models/data.model';
+
 import useWindowWidth from '../hooks/useWindowWidth';
 
 import { StyledSection } from '../components/UI/Container.styled';
 import ContentDisplay from '../components/ContentDisplay/ContentDisplay';
 import MainText from '../components/TextDisplay/MainText';
-import ExtraText from '../components/TextDisplay/ExtraText';
 import StyledTextDisplay from '../components/UI/TextDisplay.styled';
 import StyledList from '../components/UI/List.styled';
 import { TechButton } from '../components/UI/Button.styled';
 
-import data from '../assets/data.json';
-
 const Technology = () => {
-    const { technology } = data;
-    const [currTech, setCurrTech] = useState(technology[0]);
     const windowWidth = useWindowWidth();
+    const [currTech, techData, techHandler] = useData('technology');
+    const { name, description, images } = currTech as TechnologyDataType;
     const isDesktop = windowWidth > 1024;
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const currentImage = isDesktop
-        ? require(`../assets${currTech.images.portrait}`)
-        : require(`../assets${currTech.images.landscape}`);
-
-    const techDetails = { subheading: 'the terminology...', heading: currTech.name, body: currTech.description };
-
-    const techHandler = (tech: string) => {
-        const activeCrewMember = technology.find((obj) => obj.name === tech);
-        if (activeCrewMember) setCurrTech(activeCrewMember);
-    };
+    const techDetails = { subheading: 'the terminology...', heading: name, body: description };
 
     return (
         <StyledSection className="technology">
-            <ContentDisplay step="03." heading="space launch 101" path={currentImage} alt={currTech.name}>
+            <ContentDisplay
+                step="03."
+                heading="space launch 101"
+                path={isDesktop ? require(`../assets${images.portrait}`) : require(`../assets${images.landscape}`)}
+                alt={currTech.name}
+            >
                 <StyledList>
-                    {technology.map((tech, index) => (
+                    {techData.map((tech, index) => (
                         <TechButton
                             className={tech.name === currTech.name ? 'btn-active' : ''}
                             onClick={techHandler.bind(null, tech.name)}
