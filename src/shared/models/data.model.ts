97cccbb5c85e4760d77f0aacd7dfc2bef881d5d4
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-31 00:34:37 +0300

    refactor: deriving data types

---
+++ src/shared/models/data.model.ts
@@ -0,0 +1,7 @@
+import data from '../../assets/data.json';
+
+export type CrewDataType = typeof data['crew'][0];
+
+export type DestinationDataType = typeof data['destinations'][0];
+
+export type TechnologyDataType = typeof data['technology'][0];
