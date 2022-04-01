d4b0f0a65dae0a3b9b70bb8ffc66db5353201775
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-01 03:03:32 +0300

    style: adding framer variants in section and ul

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -8,20 +8,22 @@ import ExtraText from '../components/TextDisplay/ExtraText';
 import StyledTextDisplay from '../components/UI/TextDisplay.styled';
 import StyledList from '../components/UI/List.styled';
 
+import { pageAnimation, fadeAnimation } from '../shared/animations';
+
 const Destination = () => {
     const [currDestination, destinationData, destinationHandler] = useData('destinations');
     const { name, description, distance, travel, images } = currDestination as DestinationDataType;
     const textDetails = { heading: name, body: description };
 
     return (
-        <StyledSection className="destination">
+        <StyledSection className="destination" variants={pageAnimation} initial="hidden" animate="show" exit="exit">
             <ContentDisplay
                 step="01."
                 heading="pick your destination"
                 path={require(`../assets${images.webp}`)}
                 alt={name}
             >
-                <StyledList>
+                <StyledList variants={fadeAnimation}>
                     {destinationData.map((destination, index) => (
                         <li
                             className={destination.name === currDestination.name ? 'active' : ''}
